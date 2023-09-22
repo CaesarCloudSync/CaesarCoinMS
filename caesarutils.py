ca732a1a0254857f9790147be88272b6cd7ad2fd
amari <amari.lawal@gmail.com>
2023-09-23 00:42:43 +0100

    latest

--- caesarutils.py
+++
@@ -1,8 +0,0 @@
-import base64
-class CaesarUtils:
-    @staticmethod
-    def convert_thumbnail_and_cleanup(result):
-        print(result["thumbnail"])
-        del result["quotahash"],result["quoterkey"]
-        result["thumbnail"] = base64.b64encode(result["thumbnail"]).decode()
-        
